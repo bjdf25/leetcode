1f4350fc27b329559732118a455bf20bdf9c57e7
陈磊 <chlei@163.com>
2022-04-12 11:30:50 +0800

    feature# offer&&easy

---
+++ leetcode/src/main/java/com/example/leetcode/sword/LowestCommonAncestor2.java
@@ -0,0 +1,18 @@
+package com.example.leetcode.sword;
+
+import com.example.leetcode.common.TreeNode;
+
+/**
+ * II. 二叉树的最近公共祖先
+ */
+public class LowestCommonAncestor2 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root == q || root == p) return root;
+        TreeNode l = lowestCommonAncestor(root.left, p ,q);
+        TreeNode r = lowestCommonAncestor(root.right, p ,q);
+        // if (l == null && r == null） return null;
+        if (l == null) return r;
+        if (r == null) return l;
+        return root;
+    }
+}
